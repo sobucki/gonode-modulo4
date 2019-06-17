5601f6f78ecfe1e295dcfac43cfbfe4771726c49
sobucki <sobucki@gmail.com>
2019-06-17 17:11:34 -0400

    Projeto da aula em Adonis

---
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const crypto = require('crypto')
+const User = use('App/Models/User')
+const Mail = use('Mail')
+
+class ForgotPasswordController {
+  async store ({ request, response }) {
+    try {
+      const email = request.input('email')
+      const user = await User.findByOrFail('email', email)
+
+      user.token = crypto.randomBytes(10).toString('hex')
+      user.token_created_at = new Date()
+
+      await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('sobucki@gonode.com', 'Rafael | Gonode')
+            .subject('Recuperação de senha')
+        }
+      )
+    } catch (error) {
+      return response.status(error.status).send({
+        error: {
+          message: 'Alguma coisa não deu certo, talvez este email não exista'
+        },
+        errorFull: JSON.stringify(error)
+      })
+    }
+  }
+}
+
+module.exports = ForgotPasswordController
