58b7102c4aaa02c0fbc682f47456f35db4440c37
sobucki <sobucki@gmail.com>
2019-06-18 14:44:33 -0400

    Download de arquivos

---
+++ app/Controllers/Http/FileController.js
@@ -0,0 +1,78 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const File = use('App/Models/File')
+const Helpers = use('Helpers')
+
+/**
+ * Resourceful controller for interacting with files
+ */
+class FileController {
+  /**
+   * @param {object} ctx
+   * @param {Params} ctx.params
+   * @param {Response} ctx.response
+   */
+  async show ({ params, response }) {
+    try {
+      const file = await File.findByOrFail('id', params.id)
+
+      return response.download(Helpers.tmpPath(`uploads/${file.file}`))
+    } catch (error) {
+      if (error.status === 404) {
+        return response
+          .status(error.status)
+          .send({ error: { message: 'Nenhum arquivo encontrado' } })
+      } else {
+        return response
+          .status(error.status)
+          .send({ error: { message: 'Não foi possivel buscar o arquivo' } })
+      }
+    }
+  }
+
+  /**
+   * Create/save a new file.
+   * POST files
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+    try {
+      if (!request.file('file')) return
+
+      const upload = request.file('file', { size: '2mb' })
+
+      const fileName = `${Date.now()}.${upload.subtype}`
+
+      await upload.move(Helpers.tmpPath('uploads'), {
+        name: fileName
+      })
+
+      if (!upload.moved()) {
+        throw upload.error()
+      }
+
+      const file = await File.create({
+        file: fileName,
+        name: upload.clientName,
+        type: upload.type,
+        subtype: upload.subtype
+      })
+
+      return file
+    } catch (error) {
+      return response
+        .status(error.status)
+        .send({ error: { message: 'Erro no upload de arquivos' } })
+    }
+  }
+}
+
+module.exports = FileController
